56e4b37b897799bb26bf63b72eca66540fbeaa0d
Kaio Cesar <tecnico.kaio@gmail.com>
2020-05-16 21:17:12 -0300

    endpoints login and logout

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from trips.views import SignUpView
 
-router = routers.DefaultRouter()
-# router.register(r'', views.ANYTHINGELSE)
+from trips.views import SignUpView, LogInView, LogOutView
 
 urlpatterns = [
-    # path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
     path('api/sign_up/', SignUpView.as_view(), name='sign_up'),
+    path('api/log_in/', LogInView.as_view(), name='log_in'),
+    path('api/log_out/', LogOutView.as_view(), name='log_out'),
 ]
+
--- trips/tests/test_http.py
+++ trips/tests/test_http.py
@@ -5,6 +5,11 @@ from rest_framework.test import APITestCase
 
 PASSWORD = 'pAssw0rd!'
 
+def create_user(username='user@example.com', password=PASSWORD):
+    return get_user_model().objects.create_user(
+        username=username, password=PASSWORD
+    )
+
 class AuthenticationTest(APITestCase):
 
     def test_user_can_sign_up(self):
@@ -21,3 +26,18 @@ class AuthenticationTest(APITestCase):
         self.assertEqual(response.data['username'], user.username)
         self.assertEqual(response.data['first_name'], user.first_name)
         self.assertEqual(response.data['last_name'], user.last_name)
+
+    def test_user_can_log_in(self):
+        user = create_user()
+        response = self.client.post(reverse('log_in'), data={
+            'username': user.username,
+            'password': PASSWORD,
+        })
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(response.data['username'], user.username)
+
+    def test_user_can_log_out(self):
+        user = create_user()
+        self.client.login(username=user.username, password=PASSWORD)
+        response = self.client.post(reverse('log_out'))
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
